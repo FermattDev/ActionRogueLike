7909331f1e52ccbbbe12f5cd9e3ef78b9594fb9e
Matias Fernandez <matiasfernandez602@gmail.com>
2023-02-10 16:15:22 -0300

    Assigment 3 startup

--- Source/ActionRogueLike/Private/SMagicProjectile.cpp
+++ Source/ActionRogueLike/Private/SMagicProjectile.cpp
@@ -3,6 +3,8 @@
 
 #include "SMagicProjectile.h"
 #include "SAttributeComponent.h"
+#include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
+#include "Runtime/Engine/Classes/Kismet/KismetSystemLibrary.h"
 
 // Sets default values
 ASMagicProjectile::ASMagicProjectile()
@@ -10,6 +12,7 @@ ASMagicProjectile::ASMagicProjectile()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	SphereComp->OnComponentBeginOverlap.AddDynamic(this, &ASMagicProjectile::OnActorOverlap);
+	SphereComp->OnComponentHit.AddDynamic(this, &ASMagicProjectile::OnActorHit);
 }
 
 void ASMagicProjectile::OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
@@ -22,15 +25,32 @@ void ASMagicProjectile::OnActorOverlap(UPrimitiveComponent* OverlappedComponent,
 			return;
 		}
 		AttributeComp->ApplyHealthChange(-20.0f);
-		Destroy();
+		DestroyProjectile();
 	}
+}
+
+void ASMagicProjectile::OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+	DestroyProjectile();
+}
+
+void ASMagicProjectile::DestroyProjectile()
+{
+	UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), DestroyEffect, GetTransform());
+	Destroy();
 }
 
 // Called when the game starts or when spawned
 void ASMagicProjectile::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	SphereComp->IgnoreActorWhenMoving(GetInstigator(), true);
+	GetWorldTimerManager().SetTimer(TimerHandle_Destroy, FTimerDelegate::CreateLambda([&] {
+		if (IsValid(this))
+		{
+			DestroyProjectile();
+		}
+		}), 5.0f, false);
 }
 
 // Called every frame
